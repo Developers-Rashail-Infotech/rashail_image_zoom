dc6b1662632c9183af3a765ba978c8fb844d2ede
Sajal Chandaiya <sajal@rashailinfotech.co.in>
2023-02-20 09:59:06 +0530

    publishrd the package

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<rashail_image_zoom/RashailImageZoom.h>)
+#import <rashail_image_zoom/RashailImageZoom.h>
+#else
+@import rashail_image_zoom;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [RashailImageZoom registerWithRegistrar:[registry registrarForPlugin:@"RashailImageZoom"]];
+}
+
+@end
